805a774ff4c79f19983276258e4c82aa8fd8cbd4
Slawek Mazur <mazursllawek@gmail.com>
2017-08-23 14:14:55 +0200

    initial commit

---
+++ src/main/java/pl/coderslab/bookscatalog/BooksCatalogApplication.java
@@ -0,0 +1,12 @@
+package pl.coderslab.bookscatalog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BooksCatalogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BooksCatalogApplication.class, args);
+	}
+}
