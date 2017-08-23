29f1ebece1908a307dc6dae51514e6e54e932b40
Slawek Mazur <mazursllawek@gmail.com>
2017-08-23 14:39:49 +0200

    - book controller with get mapping for list

--- src/main/java/pl/coderslab/bookscatalog/controllers/BookController.java
+++ src/main/java/pl/coderslab/bookscatalog/controllers/BookController.java
@@ -1,9 +1,19 @@
 package pl.coderslab.bookscatalog.controllers;
 
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import pl.coderslab.bookscatalog.model.Book;
 
-@Controller
+import java.util.Collections;
+import java.util.List;
+
+@RestController
+@RequestMapping("/books")
 public class BookController {
 
-
+    @GetMapping
+    public List<Book> books() {
+        return Collections.emptyList();
+    }
 }
